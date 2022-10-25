eab2618d84aa5cb834682f5bf28ad18cfa98e396
Skytowz <46870222+Skytowz@users.noreply.github.com>
2022-10-25 13:58:12 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/favicon/manifest.json">
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/favicon/favicon.ico">
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta property="og:title" content="📷 kurokawa • Instagram" />
+        <meta property="og:video" content="https://v16-webapp.tiktok.com/b40949a6ee2a28210b7162813d70e811/635822de/video/tos/useast2a/tos-useast2a-pve-0068/4305c8e99bf8429b8b0974ca7b118aa1/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2418&bt=1209&cs=0&ds=3&ft=nFApP0~q12Nvj1XzmmnRfIS4HcEbIxVvYfIi9tU&mime_type=video_mp4&qs=0&rc=OjdlZzs7OTk7NTxmOTRnOkBpMzlwZDw6ZnQ2ZzMzNzczM0BhLzZeYzAtNl8xX2JfLWAzYSMvXmRlcjRncjNgLS1kMTZzcw%3D%3D&l=202210251153160102230620131300FFDB&btag=80000" />
+        <meta property="og:url" content="https://www.instagram.com/p/CkIl1GDqfYI/" />
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="📷 kurokawa • Instagram">
+        <meta name="twitter:image" content="https://scontent.cdninstagram.com/v/t51.2885-15/312982467_1309140783222457_9161026784157140177_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=9LANgDjhxB0AX_R3PiS&edm=AMO9-JQAAAAA&ccb=7-5&oh=00_AT-w9x-5dN1S0N6oI5o0_CuYZhq7HalHv-kLC9kEAb32LQ&oe=63583BB1&_nc_sid=b9f2ee">
+
+        <title>📷 kurokawa • Instagram</title>
+
+        <script language="javascript">
+            document.location.href = "https://www.instagram.com/p/CkIl1GDqfYI/"
+        </script>
+    </head>
+</html>
